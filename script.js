b978b4dd36ac4129da98a1d49e02603b9461af1d
talhako <talhakocaman38@gmail.com>
2025-04-15 21:45:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Vielen Dank für Ihre Nachricht! Wir werden uns bald bei Ihnen melden.');
+});
